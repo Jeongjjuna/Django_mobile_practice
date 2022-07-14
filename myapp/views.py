e3ecc8a471b525b72bbb3e2a80606ebef21f6fa1
Jeongjihun <wlgns2234@naver.com>
2022-07-15 01:13:22 +0900

    프로젝트 기본페이지 구성 완료

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def func_1(request):
+    aaa = {'a': 456, 'b': 789}
+    return render(request, 'pages/1.html', aaa) #1.html페이지로 aaa딕셔너리 넘겨버림
