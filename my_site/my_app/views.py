27b9fce56ead1250468cc9ab423dd420cb5cccba
Jeongjihun <wlgns2234@naver.com>
2022-07-16 02:45:19 +0900

    .

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -5,14 +5,22 @@ def example_view(request):
     # my_app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
 
+
 def page1(request):
-    # my_app/templates/my_app/example.html
-    return render(request, 'my_app/page1.html')
+
+    my_var = {
+        'first_name':'Jeong',
+        'last_name' : 'jihun',
+        'some_list':[1,2,3],
+        'some_dict':{'inside_dey' : 'inside_value'},
+    }
+    
+    return render(request, 'my_app/page1.html', context=my_var)
+
+
 
 def page2(request):
-    # my_app/templates/my_app/example.html
     return render(request, 'my_app/page2.html')
 
 def page3(request):
-    # my_app/templates/my_app/example.html
     return render(request, 'my_app/page3.html')
