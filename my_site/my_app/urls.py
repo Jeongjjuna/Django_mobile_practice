960c44c3fc01374eabee22ea2c5e008f994fba79
Jeongjihun <wlgns2234@naver.com>
2022-07-22 23:29:56 +0900

    project reset

--- my_site/my_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.example_view, name ='example_view'),
-    path('page1/', views.page1, name ='page1'),
-    path('page2/', views.page2, name ='page2'),
-    path('page3/', views.page3, name ='page3'),
-]
